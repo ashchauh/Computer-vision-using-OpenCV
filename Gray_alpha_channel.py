742465afba2c198f97287486413d32421bd4f54a
Ashish Chauhan <ashish222322@gmail.com>
2018-03-28 01:46:31 -0400

    Update Gray_alpha_channel.py

--- Gray_alpha_channel.py
+++ Gray_alpha_channel.py
@@ -1,63 +1,19 @@
 
 # coding: utf-8
-
-
-
 import numpy as np
-
-
-# In[2]:
-
-
 import cv2
-
-
-# In[5]:
-
-
 color= cv2.imread(r'C:\Users\Ashish\Desktop\Test\Messi.jpg')
-
-
-# In[6]:
-
-
 gray=cv2.cvtColor(color,cv2.COLOR_RGB2GRAY)
-
-
-# In[8]:
-
-
 cv2.imwrite("Gray.jpg", gray)
 
 
-# In[10]:
-
-
-cv2.imshow("Gray", gray)
+zv2.imshow("Gray", gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
-# In[12]:
-
-
 b= color[:,:,0]
-
-
-# In[13]:
-
-
 g=color[:,:,1]
-
-
-# In[14]:
-
-
 r=color[:,:,2]
-
-
-# In[15]:
-
 
 cv2.imshow("Blue", b)
 cv2.moveWindow("Blue",0,0)
@@ -67,13 +23,7 @@ cv2.imshow("Red", r)
 cv2.moveWindow("Red",0,100)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-# In[16]:
-
-
 rgba=cv2.merge((r,g,b,g))
 cv2.imshow("RGBA",rgba)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
