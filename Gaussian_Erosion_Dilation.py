97c49843fe3d5cb8b879b9e562a9e6e7b5b7ad68
Ashish Chauhan <ashish222322@gmail.com>
2018-03-28 01:45:15 -0400

    Update Gaussian_Erosion_Dilation.py

--- Gaussian_Erosion_Dilation.py
+++ Gaussian_Erosion_Dilation.py
@@ -6,16 +6,7 @@
 
 import numpy as np
 import cv2
-
-
-# In[3]:
-
-
 image=cv2.imread(r'C:\Users\Ashish\Desktop\Test\Messi.jpg')
-
-
-# In[4]:
-
 
 cv2.imshow("Original", image)
 blur= cv2.GaussianBlur(image,(5,55),0)
@@ -29,10 +20,6 @@ cv2.destroyAllWindows()
 # # Erosion and Dilation filter
 # ##  used to contract or expland the foreground pixels to help remove or accentuate small pixel details (speckles)
 # ## dilation--> black pixels into while pixels and erosion--> white into black
-
-# In[6]:
-
-
 kernel= np.ones((5,5),'uint8')
 dilate=cv2.dilate(image,kernel, iterations=1)
 erode= cv2.erode(image,kernel, iterations=1)
@@ -40,4 +27,3 @@ cv2.imshow("Dilate",dilate)
 cv2.imshow("Erode", erode)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
