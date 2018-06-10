0e0d44aed8b80439c2993e67bd4f883edcc3d464
Ashish Chauhan <ashish222322@gmail.com>
2018-06-10 12:00:09 -0400

    Update Scaling+and+rotating+images.py

--- Scaling+and+rotating+images.py
+++ Scaling+and+rotating+images.py
@@ -1,39 +1,13 @@
 
-# coding: utf-8
-
-# # Scaling and rotating images
-
-
+#Scaling and rotating images
 import numpy as np
 import cv2
-
-
-# In[4]:
-
-
 img= cv2.imread(r'C:\Users\Ashish\Desktop\Test\Messi.jpg')
 
-
-# In[5]:
-
-
 img_hlf= cv2.resize(img, (0,0), fx=0.5,fy=0.5)
-
-
-# In[6]:
-
-
 img_strch=cv2.resize(img, (600,600), fx=600,fy=600)
 
-
-# In[7]:
-
-
 img_strch_near=cv2.resize(img, (600,600), interpolation=cv2.INTER_NEAREST)
-
-
-# In[10]:
-
 
 cv2.imshow("Image Half",img_hlf)
 cv2.imshow("Image Stretch",img_strch)
@@ -49,9 +23,6 @@ cv2.destroyAllWindows()
 
 M=cv2.getRotationMatrix2D((0,0),-30,1)
 rotated=cv2.warpAffine(img,M, (img.shape[1],img.shape[0]))
-
-
-# In[13]:
 
 
 cv2.imshow("Rotated Image", rotated)
